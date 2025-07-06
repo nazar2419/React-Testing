8b51c2dde34747fb6547988a6f02287425f1399e
Nazar <nazarx095@gmail.com>
2025-07-06 20:15:27 +0300

    search form

---
+++ 05-search-github-users/src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Toaster } from './components/ui/toaster.tsx';
+import { StrictMode } from 'react';
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+        <Toaster/>
+    </StrictMode>
+
+);
